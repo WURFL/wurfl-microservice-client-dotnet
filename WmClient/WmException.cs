a9ae3f8208e9044b073d04685784bdea78ef49f9
mbellomi <massimiliano.bellomi@gmail.com>
2024-11-07 05:35:05 -0800

    fixes typo in comment

--- WmClient/WmException.cs
+++ WmClient/WmException.cs
@@ -47,7 +47,7 @@ namespace Wmclient
 
     public static class ExceptionHelper
     {
-        // This method preserves the original stack trace when an exception is thrown again ina a catch block
+        // This method preserves the original stack trace when an exception is thrown again in a catch block
         public static void ReThrow(Exception ex)
         {
             System.Runtime.ExceptionServices.ExceptionDispatchInfo.Capture(ex).Throw();
